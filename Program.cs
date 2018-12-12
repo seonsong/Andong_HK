f36a4f1895045f852b6142252c0a66c722e7d7eb
joo <seonsong0912@gmail.com>
2018-12-12 10:31:11 +0900

    version_1_2018_12_11

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Oracle.ManagedDataAccess.Client;
+
+namespace WindowsFormsApp2
+{
+    static class Program
+    {
+        static frmLogin login = null;
+        internal static bool logOK = false;
+        internal static bool inOK = false;
+
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new frmLogin());
+
+            login = new frmLogin();
+            Application.Run(login);
+
+            if (logOK)
+            {
+                frmMain m = new frmMain();
+                //login.Close();
+                Application.Run(m);
+            }
+            if (inOK)
+            {
+                insert newin = new insert();
+                //login.Close();
+                Application.Run(newin);
+            }
+        }
+    }
+}
